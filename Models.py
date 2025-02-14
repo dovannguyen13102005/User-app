0cbc88cd53e0f2aab399489c8c7d039a4d1a5c02
dovannguyen13102005 <dovannguyen13102005@gmail.com>
2025-02-14 22:29:48 +0700

    test

--- Models.py
+++ Models.py
@@ -9,44 +9,27 @@ class User(BaseModel):
     account: str
     password: str
 
+class NewUser:
+    def __init__(self, user_id, user_name, user_age, user_email, user_account, user_password):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.user_age = user_age
+        self.user_email = user_email
+        self.user_account = user_account
+        self.user_passWord = user_password
 class Users:
     def __init__(self):
-        self.users = []
-        self.cnt = 1
-    def get_all_users(self):
-        return self.users
-    def add_user(self, user: User):
-        new_user = {"id": self.cnt, "name": user.name, "age": user.age,"email": user.email, "account": user.account, "password": user.password}
-        if any(value["account"] == user.account for value in self.users):
-            return {"Message": "Account already exists"}
+        self.users = list()
+        self.check_existed = dict()
+        self.count = 0
+    def add_user(self, user:User):
+        new_user = NewUser(self.count, user.name, user.age, user.email, user.account, user.password)
+        if self.check_existed[user.account]:
+            return {"Message": "existed"}
         self.users.append(new_user)
-        self.cnt += 1
-        return new_user
-    def update_user(self, user_id: int, user: User):
-        new_user = {"id": user_id, "name": user.name, "age": user.age, "email": user.email}
-        for idx, value in enumerate(self.users):
-            if value["id"] == user_id:
-                self.users[idx] = new_user
-                return {"Message": "Success"}
-        return {"Message", "Error"}
-    def find_user_by_id(self, user_id: int):
-        for user in self.users:
-            if user["id"] == user_id:
-                return user
-        return {"Message": "Not Found"}
-    def find_user_by_name(self, user_account: str):
-        for user in self.users:
-            if user["account"] == user_account:
-                return user
-        return {"Message": "Not Found"}
-    def find_user_by_email(self, user_email: str):
-        for user in self.users:
-            if user["email"] == user_email:
-                return user
-            return {"Message": "Not Found"}
-    def remove_user(self, user_id: int):
-        for user in self.users:
-            if user["id"] == user_id:
-                self.users.remove(user)
-                return {"Messages": "User deleted"}
-        return {"Message": "Not Found"}
+        self.check_existed[user.account] = True
+        self.count += 1
+        return user
+    def find_user_by_id(self, user_id):
+        for value in self.users:
+            value.
