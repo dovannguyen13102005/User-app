bdd0fe2be031be4defdf1ef5029c143517a42f32
dovannguyen13102005 <dovannguyen13102005@gmail.com>
2025-02-26 23:44:34 +0700

    Update

--- App.py
+++ App.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, HTTPException, FastAPI
 from fastapi.params import Depends
+from sqlalchemy.orm import Session
 
-from Models import Users, User, NewUser
+from Models import Users, User, UserDB
 from fastapi.security import  HTTPBasic, HTTPBasicCredentials
+from Database import Base, engine, SessionLocal
 
-##router = APIRouter()
-user_manager = Users()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base.metadata.create_all(bind=engine)
+
 security = HTTPBasic()
 USERNAME = "test"
 PASSWORD = "test"
@@ -17,37 +26,44 @@ def secure(credentials : HTTPBasicCredentials = Depends(security)):
 
 app = FastAPI()
 @app.get("/get_users")
-async def get_user(test = Depends(secure)):
+async def get_user(test = Depends(secure), db: Session = Depends(get_db)):
     if not test:
         return {"Message": "Can't access"}
+    user_manager = Users(db)
     return user_manager.get_all_users()
 @app.post("/add_user")
-async def create_user(user: User, test = Depends(secure)):
+async def create_user(user: User, test = Depends(secure), db: Session = Depends(get_db)):
     if not test:
         return {"Message": "Can't access"}
+    user_manager = Users(db)
     return user_manager.add_user(user)
 @app.get("/find_user_by_id/{user_id}")
-async def get_user(user_id: int, test = Depends(secure)):
+async def get_user(user_id: int, test = Depends(secure), db: Session = Depends(get_db)):
     if not test:
         return {"Message": "Can't access"}
+    user_manager = Users(db)
     return user_manager.find_user_by_id(user_id)
 @app.delete("/remove_user/{user_id}")
-async def delete_user(user_id: int, test = Depends(secure)):
+async def delete_user(user_id: int, test = Depends(secure), db: Session = Depends(get_db)):
     if not test:
         return {"Message": "Can't access"}
+    user_manager = Users(db)
     return user_manager.remove_user(user_id)
 @app.put("/update_user/{user_id}")
-async def update_user(user_id: int, user: User, test = Depends(secure)):
+async def update_user(user_id: int, user: User, test = Depends(secure), db: Session = Depends(get_db)):
     if not test:
         return {"Message": "Can't access"}
+    user_manager = Users(db)
     return user_manager.update_user(user_id, user)
 @app.get("/find_user_by_account/{user_account}")
-async def get_user(user_account: str, test = Depends(secure)):
+async def get_user(user_account: str, test = Depends(secure), db: Session = Depends(get_db)):
     if not test:
         return {"Message": "Can't access"}
+    user_manager = Users(db)
     return user_manager.find_user_by_account(user_account)
 @app.get("/find_user_by_email/{user_email}")
-async def get_user(user_email: str, test = Depends(secure)):
+async def get_user(user_email: str, test = Depends(secure), db: Session = Depends(get_db)):
     if not test:
         return {"Message": "Can't access"}
+    user_manager = Users(db)
     return user_manager.find_user_by_email(user_email)
--- main.py
+++
@@ -1,5 +0,0 @@
-##from App import router
-from fastapi import FastAPI
-
-app = FastAPI()
-##app.include_router(router)
